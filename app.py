31fa8cedcec6f7d337d5e62164409ad9936951f6
Diego Rosso <diegorosso1988@gmail.com>
2022-09-12 14:54:57 -0300

    ajlaks

--- app.py
+++ app.py
@@ -12,23 +12,26 @@ CORS(app)
 
 db = mongo.db.users #colección de usuarios
 
-@app.route("/")
-def index():
-    return  "╰(*°▽°*)╯"
+@app.route('/block', methods=['GET'])
+def blockUser():
+    return "Hola"
 
-@app.route('/users', methods=['POST']) # Crea rutas para crear usuarios 
+@app.route('/users', methods=['POST']) # Crear usuarios 
 def createUser():
+    try:
+        data = request.get_json()
+        if ('name' not in data or data['name'] == '' or 'email' not in data or 'password' not in data):
+            return {"msg": "Missing parameters"}, 400
     
-    data = request.get_json()
-    if ('name' not in data or data['name'] == '' or 'email' not in data or 'password' not in data):
-        return {"msg": "Missing parameters"}, 400
-   
-   
-    id = db.insert_one({         #Guardar datos con metodo Insert
-        'name': data['name'],
-        'email': data['email'],
-        'password': data['password']
-     })
+    
+        id = db.insert_one({         #Guardar datos con metodo Insert
+            'name': data['name'],
+            'email': data['email'],
+            'password': data['password'],
+            'enable': data['enable']
+        })
+    except:
+        return "Hubo un error en la conexion", 409
 
     return jsonify(str(id.inserted_id)) #str convierte el objeto en formato string  
                  
@@ -44,10 +47,11 @@ def getUsers():
                 '_id': str(ObjectId(doc['_id'])), #ID en formato string
                 'name': doc['name'],
                 'email': doc['email'],
-                'password': doc['password']
+                'password': doc['password'],
+                'enable': doc['enable']
             })
     except:
-        return "No se tar a mongo", 408
+        return "No se pudo conectar a mongo", 408
 
 
     return jsonify(users)
@@ -56,28 +60,45 @@ def getUsers():
 def getUser(id):
     try:
         user = db.find_one({'_id':ObjectId(id)})
+        if not user:
+            return "El id no existe", 404
     except:
-        return "id no existe", 404
+        return "Hubo un error", 409
    
     return jsonify({
         '_id': str(ObjectId(user['_id'])),
         'name': user['name'],
         'email': user['email'],
-        'password': user['password']        
+        'password': user['password'],
+        'enable': user['enable']        
     })
 
 @app.route('/users/<id>', methods=['DELETE']) # Crea rutas para eliminar usuarios 
 def deleteUser(id):
-    db.delete_one({'_id': ObjectId(id)}) #Elimina el primer dato que coincide con la búsqueda. 
+    try:
+        res = db.delete_one({'_id': ObjectId(id)}) #Elimina el primer dato que coincide con la búsqueda. 
+        if not res:
+            return "El id no existe", 404
+    except:
+        return "Hubo un error en la conexion", 409
+
     return jsonify({'msg': 'User delete'})
 
 @app.route('/users/<id>', methods=['PUT']) # Crea rutas para poder editar usuario 
 def updeteUser(id):
-    db.update_one({'_id': ObjectId(id)}, {'$set':{
-        'name': request.json['name'],
-        'email': request.json['email'],
-        'password': request.json['password']
-    }})
+    try:
+        user = db.update_one({'_id': ObjectId(id)}, {'$set':{
+            'name': request.json['name'],
+            'email': request.json['email'],
+            'password': request.json['password'],
+            'enable': request.json['enable'] 
+        }})
+        if not user:
+            return "El id no existe", 404
+
+    except:
+       return "Hubo un error en la conexion", 409
+
     return jsonify({'msg': 'User update'})
 
 
